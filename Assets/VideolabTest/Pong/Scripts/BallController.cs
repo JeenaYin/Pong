49045ba237b9ee22c2b82ffbd9e6dc0daf08c6bf
Jeena Yin <yinqiya@gmail.com>
2019-08-07 18:14:07 -0400

    every

---
+++ Assets/VideolabTest/Pong/Scripts/BallController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody rb;
+    public int id;
+    private bool launched;
+    float randomRange = 1.5f;
+    Vector3 dampGoal;
+    Vector3 placeHolder;
+
+    Vector3 crazyLaunchVel;
+    Vector3 crazyLaunchLoc;
+
+    public void LaunchBall(float waittime, Vector3 loc, Vector3 vel)
+    {
+        launched = false;
+        rb = GetComponent<Rigidbody>();
+        crazyLaunchLoc = loc;
+        crazyLaunchVel = vel;
+        StartCoroutine("Launch", waittime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (launched)
+            rb.velocity = SlowDown(rb.velocity);
+        if (Mathf.Abs(transform.position.x) > 10f || Mathf.Abs(transform.position.y) > 7f)
+        {
+            if(transform.position.x < 0)
+                GameController.controller.BallMinusOne(1, id);
+            else
+                GameController.controller.BallMinusOne(0, id);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Vector3 newVector = new Vector3();
+        newVector = rb.velocity;
+
+        if (collision.gameObject.CompareTag("Bats") 
+            || collision.gameObject.CompareTag("Bumper") 
+            || collision.gameObject.CompareTag("Boundaries"))
+        {
+            if(collision.relativeVelocity.sqrMagnitude < 30f)
+            {
+                newVector = Vector3.Scale(rb.velocity, new Vector3(2.2f, 2.2f, 0f));
+            }
+            else if (collision.relativeVelocity.sqrMagnitude < 40f)
+            {
+                newVector = Vector3.Scale(rb.velocity, new Vector3(2f, 2f, 0f));
+
+            } else if(collision.relativeVelocity.sqrMagnitude < 60f)
+            {
+                newVector = Vector3.Scale(rb.velocity, new Vector3(1.7f, 1.7f, 0f));
+            }
+            else if (collision.relativeVelocity.sqrMagnitude < 80f)
+            {
+                newVector = Vector3.Scale(rb.velocity, new Vector3(1.5f, 1.5f, 0f));
+            }
+        }
+        rb.velocity = Amplify(newVector);
+        dampGoal = Vector3.Scale(rb.velocity, new Vector3(0.7f, 0.7f, 0));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameController.controller.BallCollision(collision);
+    }
+    
+    IEnumerator Launch(float waittime)
+    {
+        yield return new WaitForSeconds(waittime);
+        launched = true;
+
+        if(crazyLaunchVel.magnitude != 0)
+        {
+            rb.velocity = crazyLaunchVel;
+        }
+        else
+        {
+            int x = Random.Range(0, 2);
+            int y = Random.Range(0, 3);
+            Vector3 launchDir = new Vector3
+            {
+                x = x == 0 ? 7f : -7f,
+                y = y == 0 ? 7f : -7f
+            };
+            launchDir.y = y == 2 ? 0f : launchDir.y;
+            launchDir.x = y == 2 ? (launchDir.x * 1.414f) : launchDir.x;
+            rb.velocity = launchDir;
+        }
+        dampGoal = Vector3.Scale(rb.velocity, new Vector3(1f, 1f, 0));
+    }
+
+    Vector3 Amplify(Vector3 original)
+    {
+        Vector3 newVector = original;
+        float absX = Mathf.Abs(original.x);
+        float absY = Mathf.Abs(original.y);
+
+        if (absX < 2.7f)
+        {
+            if (absX < 0.5f)
+            {
+                newVector = new Vector3(Random.Range(-randomRange, randomRange), original.y, 0f);
+            }
+            else
+                newVector = Vector3.Scale(original, new Vector3(1.5f, 1f, 0f));
+        }
+
+        if (absY < 1f)
+        {
+            if (absY < 0.5f)
+            {
+                newVector = new Vector3(original.x, Random.Range(-randomRange, randomRange), 0f);
+            }
+            else 
+                newVector = Vector3.Scale(original, new Vector3(1.5f, 2f, 0f));
+        }
+        newVector = ClampMag(newVector, 12, 3);
+        return newVector;
+    }
+
+    Vector3 SlowDown(Vector3 original)
+    {
+        Vector3 newVector = original;
+        newVector = Vector3.SmoothDamp(newVector, dampGoal, ref placeHolder, 0.8f);
+        newVector = Vector3.ClampMagnitude(newVector, 25);
+        return newVector;
+    }
+
+    public static Vector3 ClampMag(Vector3 v, float max, float min)
+    {
+        double sm = v.sqrMagnitude;
+        if (sm > (double)max * (double)max) return v.normalized * max;
+        else if (sm < (double)min * (double)min) return v.normalized * min;
+        return v;
+    }
+}
---
+++ Assets/VideolabTest/Pong/Scripts/GameController.cs
@@ -0,0 +1,334 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController controller;
+    //public TextMeshProUGUI Score;
+    //public CanvasGroup RightWinCG;
+    //public CanvasGroup LeftWinCG;
+    public int leftScore = 0;
+    public int rightScore = 0;
+    public GameObject leftBat;
+    public GameObject rightBat;
+    public bool crazyMode = false;
+    public readonly int crazyMaxBallNum = 8;
+    public int curBallNum = 0;
+
+    private GameObject[] Balls;
+    public GameObject Ball0;
+    public GameObject Ball1;
+    public GameObject Ball2;
+    public GameObject Ball3;
+    public GameObject Ball4;
+    public GameObject Ball5;
+    public GameObject Ball6;
+    public GameObject Ball7;
+
+    private GameObject[] Bumpers;
+    public GameObject Bumper0;
+    public GameObject Bumper1;
+    public GameObject Bumper2;
+    public GameObject Bumper3;
+
+    private Vector3[] BumperPos;
+    private Vector3[] BumperVel;
+    private int numBumper;
+    private int maxBumper = 4;
+    private float fadeTime = 0.3f;
+    private int hitCounter1 = 0;
+    private int hitCounter2 = 0;
+
+    private Vector3 crazyLaunchLoc;
+    private Vector3 crazyLaunchVel;
+    //private int maxScore = 20;
+    //private bool gameOver = false;
+
+    private float smoothTime = 0.1f;
+
+    private void Awake()
+    {
+        if (controller != null && controller != this)
+            Destroy(this.gameObject);
+        else
+        {
+            controller = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        //LeftWinCG.alpha = 0;
+        //RightWinCG.alpha = 0;
+        Bumpers = new GameObject[maxBumper];
+        BumperPos = new Vector3[maxBumper];
+        BumperVel = new Vector3[maxBumper];
+        Bumpers[0] = Bumper0;
+        Bumpers[1] = Bumper1;
+        Bumpers[2] = Bumper2;
+        Bumpers[3] = Bumper3;
+        for (int i = 0; i < Bumpers.Length; i++)
+        {
+            Bumpers[i].SetActive(false);
+        }
+        for (int i = 0; i < maxBumper; i++)
+        {
+            BumperPos[i] = new Vector3(Random.Range(-7f, 7f), Random.Range(-5f, 5f), 0);
+        }
+        Balls = new GameObject[crazyMaxBallNum];
+        Balls[0] = Ball0;
+        Balls[1] = Ball1;
+        Balls[2] = Ball2;
+        Balls[3] = Ball3;
+        Balls[4] = Ball4;
+        Balls[5] = Ball5;
+        Balls[6] = Ball6;
+        Balls[7] = Ball7;
+        for (int i = 0; i < Balls.Length; i++)
+        {
+            Balls[i].SetActive(false);
+        }
+        SpawnFirstBall();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        leftBat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        rightBat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            leftBat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 8f, 0f);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            leftBat.GetComponent<Rigidbody>().velocity = new Vector3(0f, -8f, 0f);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            rightBat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 8f, 0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rightBat.GetComponent<Rigidbody>().velocity = new Vector3(0f, -8f, 0f);
+        }
+
+        for (int i = 0; i < maxBumper; i++)
+            UpdateEachBumperPos(i);
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        //Score.text = leftScore.ToString() + " : " + rightScore.ToString();
+        //curBallNum = NumBalls();
+    }
+
+    private void SpawnFirstBall()
+    {
+        Debug.Log("Spawn first ball");
+        for (int i = 0; i < Balls.Length; i++)
+        {
+            Balls[i].SetActive(false);
+        }
+        Balls[0].SetActive(true);
+        
+        Balls[0].GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        Balls[0].transform.position = new Vector3(0, 0, 0);
+        Balls[0].GetComponent<BallController>().id = 0;
+        Balls[0].GetComponent<BallController>().LaunchBall(1, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+    }
+
+    private void UpdateEachBumperPos(int idx)
+    {
+        if (Bumpers[idx] != null)
+            Bumpers[idx].transform.position = Vector3.SmoothDamp(Bumpers[idx].transform.position, BumperPos[idx], ref BumperVel[idx], smoothTime);
+    }
+
+    private void SpawnNewBumper()
+    {
+        //if(!gameOver)
+        //{
+            Bumpers[numBumper].SetActive(true);
+            Bumpers[numBumper].transform.position = new Vector3(Random.Range(-7f, 7f), Random.Range(-5f, 5f), 0);
+            numBumper++;
+        //}
+    }
+
+    public void BallCollision(Collision collision)
+    {
+        if (numBumper == 0)
+        {
+            hitCounter1++;
+            if(hitCounter1 > 5)
+            {
+                int ran = Random.Range(0, 10);
+                if (ran > 2)
+                {
+                    SpawnNewBumper();
+                }
+            }
+        }
+        if (numBumper > 0 && collision.gameObject.CompareTag("Bumper"))
+        {
+            if (numBumper < maxBumper)
+            {
+                hitCounter2++;
+                if(numBumper == 1)
+                {
+                    SpawnNewBumper();
+                }
+                else
+                {
+                    int ran = Random.Range(0, 10);
+                    if(numBumper > 2)
+                    {
+                        if (ran > 5)
+                            SpawnNewBumper();
+                    }
+                    else if(ran > 4)
+                    {
+                        SpawnNewBumper();
+                    }
+                }
+            }
+            else if (numBumper == maxBumper)
+            {
+                curBallNum = NumBalls();
+                int ran = Random.Range(0, 10);
+                if (ran > 5  && curBallNum < 3)
+                {
+                    //Debug.Log("Trigger pattern");
+                    for (int i = 0; i < maxBumper; i++)
+                    {
+                        BumperPos[i] = new Vector3(Random.Range(-7f, 7f), Random.Range(-5f, 5f), 0);
+                    }
+                }
+                else
+                {
+                    //Debug.Log("Trigger effect");
+                    if(!crazyMode)
+                    {
+                        StartCoroutine("EnterCrazyMode");
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator EnterCrazyMode()
+    {
+        yield return new WaitForSeconds(0.05f);
+        crazyLaunchVel = Balls[0].GetComponent<Rigidbody>().velocity;
+        crazyLaunchLoc = Balls[0].GetComponent<Transform>().position;
+        for (int i = 1; i < Balls.Length ; i++)
+        {
+            StartCoroutine("LaunchNewBall", (float)(i * 0.2f));
+        }
+        crazyMode = true;
+    }
+
+    public void BallMinusOne(int winner, int id)
+    {
+        Balls[id].SetActive(false);
+
+        curBallNum = NumBalls();
+
+        if (curBallNum == 0)
+        {
+            crazyMode = false;
+            SpawnFirstBall();
+        }
+        //if (!gameOver)
+        //{
+            
+        //    if (winner == 1)
+        //        rightScore++;
+        //    else
+        //        leftScore++;
+        //    if (leftScore == maxScore)
+        //    {
+        //        LeftWin();
+        //    }
+        //    if (rightScore == maxScore)
+        //    {
+        //        RightWin();
+        //    }
+        //}
+    }
+
+    IEnumerator LaunchNewBall (float waittime)
+    {
+        int idx = (int) (waittime / 0.2f);
+        Debug.Log("Launch new ball " + idx);
+        yield return new WaitForSeconds(waittime);
+        Balls[idx].SetActive(true);
+        Balls[idx].transform.position = crazyLaunchLoc;
+        Balls[idx].GetComponent<BallController>().id = idx;
+        Balls[idx].GetComponent<BallController>().LaunchBall(0, crazyLaunchLoc, crazyLaunchVel);
+        Debug.Log("current ballnum " + curBallNum);
+    }
+
+    //void LeftWin()
+    //{
+    //    gameOver = true;
+    //    StartCoroutine("FadeInCG", LeftWinCG);
+    //    for(int i = 0; i < Balls.Length; i++)
+    //    {
+    //        Balls[i].SetActive(false);
+    //    }
+    //    for (int i = 0; i < Bumpers.Length; i++)
+    //    {
+    //        Bumpers[i].SetActive(false);
+    //    }
+    //}
+
+    //void RightWin()
+    //{
+    //    gameOver = true;444444444444444444444444444444444444444444444444444444
+    //    StartCoroutine("FadeInCG", RightWinCG);
+    //    for (int i = 0; i < Balls.Length; i++)
+    //    {
+    //        Balls[i].SetActive(false);
+    //    }
+    //    for (int i = 0; i < Bumpers.Length; i++)
+    //    {
+    //        Bumpers[i].SetActive(false);
+    //    }
+    //}
+
+    private int NumBalls()
+    {
+        int num = 0;
+        foreach(GameObject ball in Balls)
+        {
+            if (ball.activeSelf)
+                num++;
+        }
+        return num;
+    }
+
+    private IEnumerator FadeOutCG(CanvasGroup cg)
+    {
+        while (cg.alpha > 0)
+        {
+            cg.alpha -= Time.deltaTime / fadeTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        cg.alpha = 0;
+    }
+
+    private IEnumerator FadeInCG(CanvasGroup cg)
+    {
+        while (cg.alpha < 1)
+        {
+            cg.alpha += Time.deltaTime / fadeTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        cg.alpha = 1;
+    }
+}
